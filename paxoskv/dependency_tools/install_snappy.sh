b4a1c830419a13d9271ba8fd135424e153e4c030
zjyjer <zjyjer@gmail.com>
2017-09-16 15:32:45 +0800

    using git submodule to fetch deps source code.

--- paxoskv/dependency_tools/install_snappy.sh
+++ paxoskv/dependency_tools/install_snappy.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
-rm -rf snappy
-mkdir snappy
-cd snappy
-wget https://github.com/google/snappy/archive/1.1.6.tar.gz -O snappy-1.1.6.tar.gz
-tar zxvf snappy-1.1.6.tar.gz
-cd snappy-1.1.6
+#rm -rf snappy
+#mkdir snappy
+#cd snappy
+#wget https://github.com/google/snappy/archive/1.1.6.tar.gz -O snappy-1.1.6.tar.gz
+#tar zxvf snappy-1.1.6.tar.gz
+#cd snappy-1.1.6
 
+cd snappy
 mkdir build
 cd build && cmake ../ && make -j2
 sudo make install
