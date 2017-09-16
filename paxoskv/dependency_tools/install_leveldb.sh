9b7015b6afc49aaa90b6221232ac31964e2de644
zjyjer <zjyjer@gmail.com>
2017-09-16 17:16:19 +0800

    fixup! add debug info for install_deps.sh.

--- paxoskv/dependency_tools/install_leveldb.sh
+++ paxoskv/dependency_tools/install_leveldb.sh
@@ -3,4 +3,4 @@
 set -ex
 base_dir=$(cd `dirname $0`;pwd)
 cd $(dirname $base_dir)/leveldb
-#make -j2 && sudo cp out-shared/libleveldb.so* /usr/local/lib/ && sudo cp out-static/libleveldb.a /usr/local/lib/ && sudo cp include/leveldb /usr/local/include/ -r && sudo ldconfig
+make -j2 && sudo cp out-shared/libleveldb.so* /usr/local/lib/ && sudo cp out-static/libleveldb.a /usr/local/lib/ && sudo cp include/leveldb /usr/local/include/ -r && sudo ldconfig
