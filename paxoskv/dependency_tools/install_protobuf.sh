8beb6b90f114a1b81e98a67a7ea84d0c787a3c95
zjyjer <zjyjer@gmail.com>
2017-09-16 15:50:53 +0800

    commit

--- paxoskv/dependency_tools/install_protobuf.sh
+++ paxoskv/dependency_tools/install_protobuf.sh
@@ -1,12 +1,6 @@
 #!/bin/sh
 
-rm -rf protobuf
-mkdir protobuf
 cd protobuf
-wget https://github.com/google/protobuf/releases/download/v2.6.1/protobuf-2.6.1.tar.gz -O protobuf-2.6.1.tar.gz
-tar zxvf protobuf-2.6.1.tar.gz
-cd protobuf-2.6.1
-
 ./configure
 make -j3
 make check
