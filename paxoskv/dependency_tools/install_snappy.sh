000db36278e7aeb7f7144b153fabf5ab656eb0da
zjyjer <zjyjer@gmail.com>
2017-09-17 11:43:43 +0800

    fix compile on travis-ci.

--- paxoskv/dependency_tools/install_snappy.sh
+++ paxoskv/dependency_tools/install_snappy.sh
@@ -3,6 +3,6 @@
 set -ex
 base_dir=$(cd `dirname $0`;pwd)
 cd $(dirname $base_dir)/snappy
-mkdir build
-cd build && cmake ../ && make -j2
+mkdir build && cd build 
+export CXXFLAGS="-fPIC" && cmake ../ && make -j2
 sudo make install && sudo ldconfig
