76873bc65e2f90db4a0e0365400234c16891a292
zjyjer <zjyjer@gmail.com>
2017-09-17 13:54:41 +0800

    fix

--- paxoskv/dependency_tools/install_protobuf.sh
+++ paxoskv/dependency_tools/install_protobuf.sh
@@ -3,4 +3,4 @@
 set -ex
 base_dir=$(cd `dirname $0`;pwd)
 cd $(dirname $base_dir)/protobuf
-./configure && make -j3 && make check && sudo make install && sudo ldconfig
+./autogen.sh && ./configure && make -j3 && make check && sudo make install && sudo ldconfig
